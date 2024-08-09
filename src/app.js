d70d9267db54e6da2f4eabe2a7cf2e09f05ffc36
AdrielD23 <162411904+AdrielD23@users.noreply.github.com>
2024-08-09 23:54:37 +0000

    Changes to main

--- src/app.js
+++ src/app.js
@@ -11,9 +11,12 @@ let noun = ["jogger", "racoon"];
 
 window.onload = function() {
   for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-      for (let k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
+
+  for (let j = 0; j < adj.length; j++) {
+
+  for (let k = 0; k < noun.length; k++) {
+    
+  console.log(pronoun[i] + adj[j] + noun[k] + ".com");
       }
     }
   }
